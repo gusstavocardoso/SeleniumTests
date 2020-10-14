96ba054bbd8c8956eb493d128fae633d715cc238
Gustavo Cardoso <gusstavocardoso@gmail.com>
2020-10-13 21:31:40 -0300

    Add Tests

--- src/test/java/CursoSelenium/TestOne.java
+++ src/test/java/CursoSelenium/TestOne.java
@@ -1,18 +1,39 @@
 package cursoSelenium;
 
+import org.junit.After;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
 
 public class TestOne {
-    @Test
-    public void navigateGoogle(){
-        System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
+
+    String driverChrome = "C:\\Drivers\\chromedriver.exe";
+    String driverGecko = "C:\\Drivers\\geckodriver.exe";
+    WebDriver driver;
+
+    @Before
+    public void startBrowser(){
+        System.setProperty("webdriver.chrome.driver", driverChrome);
+        System.setProperty("webdriver.gecko.driver", driverGecko);
+        // driver = new FirefoxDriver();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
+    }
+
+    @Test
+    public void navigateGoogle() {
         driver.get("https://www.google.com");
         Assert.assertEquals("Google", driver.getTitle());
+    }
+
+    @After
+    public void endTest(){
         driver.quit();
     }
+
 }
