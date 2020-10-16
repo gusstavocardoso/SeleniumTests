3c84e15d5c662ac4db295e2f11b9bebd1a5d16bd
Gustavo Cardoso <gusstavocardoso@gmail.com>
2020-10-16 16:46:05 -0300

    Selenium Commit 4

---
+++ src/test/java/CursoSelenium/TestCadastro.java
@@ -0,0 +1,10 @@
+package cursoSelenium;
+
+import org.junit.Test;
+
+public class TestCadastro {
+    @Test
+    public void deveFazerCadastro(){
+
+    }
+}
---
+++ src/test/java/CursoSelenium/TestWindowAndFrame.java
@@ -0,0 +1,65 @@
+package cursoSelenium;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestWindowAndFrame {
+    @Test
+    public void frames(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+
+        driver.switchTo().frame("frame1");
+        driver.findElement(By.id("frameButton")).click();
+        Alert alerta = driver.switchTo().alert();
+        String msg = alerta.getText();
+        Assert.assertEquals("Frame OK!", msg);
+        alerta.accept();
+
+        driver.switchTo().defaultContent();
+        driver.findElement(By.id("elementosForm:nome")).sendKeys("msg");
+
+        driver.quit();
+    }
+
+    @Test
+    public void window(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+
+        driver.findElement(By.id("buttonPopUpEasy")).click();
+        driver.switchTo().window("Popup");
+        driver.findElement(By.tagName("textarea")).sendKeys("Deu certo?");
+        driver.close();
+        driver.switchTo().window("");
+        driver.findElement(By.tagName("textarea")).sendKeys("E agora?");
+
+        driver.quit();
+    }
+
+    @Test
+    public void windowHandle(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("file:///" + System.getProperty("user.dir") + "/src/main/resources/componentes.html");
+
+        driver.findElement(By.id("buttonPopUpHard")).click();
+        System.out.println(driver.getWindowHandle());
+        System.out.println(driver.getWindowHandles());
+        driver.switchTo().window((String) driver.getWindowHandles().toArray()[1]);
+        driver.findElement(By.tagName("textarea")).sendKeys("Deu certo?");
+        driver.switchTo().window((String) driver.getWindowHandles().toArray()[0]);
+        driver.findElement(By.tagName("textarea")).sendKeys("E agora");
+
+        driver.quit();
+    }
+}
