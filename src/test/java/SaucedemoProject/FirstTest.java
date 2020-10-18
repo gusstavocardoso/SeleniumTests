898331ddb45ebc15b7d22567d4b5c02b90cc8756
Gustavo Cardoso <gusstavocardoso@gmail.com>
2020-10-18 18:10:03 -0300

    Selenium Commit 5

---
+++ src/test/java/SaucedemoProject/FirstTest.java
@@ -0,0 +1,67 @@
+package saucedemoproject;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class FirstTest {
+
+    WebDriver driver;
+
+    @Before
+    public void startTest(){
+        System.setProperty("webdriver.chrome.driver", "C:\\Drivers\\chromedriver.exe");
+        driver=new ChromeDriver();
+        driver.navigate().to("https://www.saucedemo.com/");
+    }
+
+//    @After
+//    public void endTest(){
+//        driver.quit();
+//    }
+
+    @Test
+    public void deveComprarProdutos(){
+        PageObjects page = new PageObjects(driver);
+        page.loginPage("standard_user", "secret_sauce");
+
+        WebElement[] produtos = {
+                driver.findElement(new By.ByCssSelector
+                        ("#inventory_container > div > div:nth-child(1) > div.pricebar > button")),
+                driver.findElement(new By.ByCssSelector
+                        ("#inventory_container > div > div:nth-child(2) > div.pricebar > button")),
+                driver.findElement(new By.ByCssSelector
+                        ("#inventory_container > div > div:nth-child(3) > div.pricebar > button")),
+                driver.findElement(new By.ByCssSelector
+                        ("#inventory_container > div > div:nth-child(4) > div.pricebar > button")),
+                driver.findElement(new By.ByCssSelector
+                        ("#inventory_container > div > div:nth-child(5) > div.pricebar > button")),
+                driver.findElement(new By.ByCssSelector
+                        ("#inventory_container > div > div:nth-child(6) > div.pricebar > button"))
+        };
+        for (WebElement produto: produtos) {
+            produto.click();
+        }
+
+        WebElement contador = driver.findElement(By.className("shopping_cart_badge"));
+        Assert.assertEquals("6", contador.getText());
+
+        driver.findElement(By.className("fa-shopping-cart")).click();
+
+        driver.findElement(By.className("checkout_button")).click();
+
+        page.checkoutPage("Selenium", "Webdriver", "12345678");
+
+        WebElement resumo = driver.findElement(By.className("subheader"));
+        Assert.assertEquals("Checkout: Overview", resumo.getText());
+
+        driver.findElement(By.className("btn_action")).click();
+
+        WebElement confirmacao = driver.findElement(By.className("complete-header"));
+        Assert.assertEquals("THANK YOU FOR YOUR ORDER", confirmacao.getText());
+    }
+}
---
+++ src/test/java/SaucedemoProject/PageObjects.java
@@ -0,0 +1,28 @@
+package saucedemoproject;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class PageObjects {
+
+    WebDriver driver;
+
+    public PageObjects(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    public void loginPage(String nome, String senha){
+        driver.findElement(By.id("user-name")).sendKeys(nome);
+        driver.findElement(By.id("password")).sendKeys(senha);
+
+        driver.findElement(By.id("login-button")).click();
+    }
+
+    public void checkoutPage(String nome, String sobrenome, String cep){
+        driver.findElement(By.id("first-name")).sendKeys(nome);
+        driver.findElement(By.id("last-name")).sendKeys(sobrenome);
+        driver.findElement(By.id("postal-code")).sendKeys(cep);
+
+        driver.findElement(By.className("cart_button")).click();
+    }
+}
