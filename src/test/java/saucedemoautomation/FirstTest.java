5d74b87d40b63469572a40488e065db7d30a889a
Gustavo Cardoso <gusstavocardoso@gmail.com>
2020-10-18 21:27:40 -0300

    Page Objects

--- src/test/java/saucedemoautomation/FirstTest.java
+++ src/test/java/saucedemoautomation/FirstTest.java
@@ -1,4 +1,4 @@
-package saucedemoproject;
+package saucedemoautomation;
 
 import org.junit.After;
 import org.junit.Assert;
--- src/test/java/saucedemoautomation/PageObjects.java
+++ src/test/java/saucedemoautomation/PageObjects.java
@@ -1,4 +1,4 @@
-package saucedemoproject;
+package saucedemoautomation;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
